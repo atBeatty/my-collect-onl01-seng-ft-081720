f6665fd214a815d53d7ddbe1d078787ad5014283
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-19 22:51:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,8 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 
 def my_collect(array)
-  collection = ['ruby', 'javascript', 'python', 'objective-c']
-  my_collect(collection) do |lang|
+  array do |lang|
   lang.upcase
-end
-
+  end
 end
